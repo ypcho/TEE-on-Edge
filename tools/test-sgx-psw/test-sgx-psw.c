1b9c9c97a7f9d7f9e44dc07f40b6265989bf5755
Lee Kyeong Ryong <kryongleesec@gmail.com>
2020-12-09 18:38:19 +0900

    test-sgx-psw add dlopen function use

--- tools/test-sgx-psw/test-sgx-psw.c
+++ tools/test-sgx-psw/test-sgx-psw.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <dlfcn.h>
 
 //based on the following reference from intel
 //reference: https://software.intel.com/content/www/us/en/develop/articles/properly-detecting-intel-software-guard-extensions-in-your-applications.html
 
 void check_sgx(){
+	void * sgx_urts = dlopen("/home/gylee/infotracking/sgxsdk/lib64/libsgx_urts.so", RTLD_NOW);
 
+	dlclose(sgx_urts);
 }
 
 int main(){
