83ed46a51b02e3bacce3ebdd32980e0bbc2425f9
Lee Kyeong Ryong <kryongleesec@gmail.com>
2020-12-09 18:29:00 +0900

    test-sgx-psw add function check_sgx

--- tools/test-sgx-psw/test-sgx-psw.c
+++ tools/test-sgx-psw/test-sgx-psw.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+void check_sgx(){
+
+}
+
 int main(){
 	printf("This is a program to test the behavior of Intel SGX\n");
 	printf("including the correct functionality of SDK and PSW\n");
+
+	check_sgx();
 }
